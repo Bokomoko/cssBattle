1c76ede6954c9094ee87681dcc69c5a6cefe1cd0
Joao Eurico <joao.eurico@live.com>
2022-12-06 10:11:59 -0300

    setInterval must be used with usememo

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ function App() {
   const [clicked,setClicked] = useState<Clicked[]>([])
   const [undo, setUndo] = useState<Clicked[]>([])
   const [line, setLine] = useState<number>(0)
+  const [col, setcol] = useState<number>(0)
+  // the followint setInterval is causing problems.
+  // probably, it should be called only once, but it is called every time the component is rendered
+  const myTimer = setInterval(() => {
+    setLine(()=>line + 1)
+    setcol(()=>col + 1)
+  },5000)
 
   const handleClick = ({clientX, clientY}:React.MouseEvent<HTMLDivElement>) => {
     setClicked([...clicked, {clientX, clientY}])
@@ -30,6 +37,7 @@ function App() {
   return (<>
       <button disabled={ clicked.length ===0} onClick={handleUndo} className="undo">Undo</button>
       <button disabled={ undo.length ===0} onClick={handleRedo} className="undo">Redo</button>
+      <div className="bolinha" style={{top: line, left: col}}></div>
       <div className="App" onClick={handleClick}>
       {clicked.map((click:Clicked, index:number) => (
         <div key={index} className="click" style={{top: click.clientY, left: click.clientX}}>
